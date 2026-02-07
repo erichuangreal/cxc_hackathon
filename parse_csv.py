8f4d0599a654fa07de6227f8fe1e4f7338b7fd5b
Eric <huangheeh@gmail.com>
2026-02-07 00:07:58 -0500

    changed features

--- parse_csv.py
+++ parse_csv.py
@@ -2,9 +2,8 @@ import pandas as pd
 
 df = pd.read_csv('forest_health_data_with_target.csv')
 
-features = df[['Elevation', 'Temperature', 'Humidity', 'Soil_TN', 'Soil_TP', 'Fire_Risk_Index']]
-
-features.columns = ['Elevation', 'Temperature', 'Humidity', 'Soil_TN', 'Soil_TP', 'Fire_Risk_Index']
+# features = df[['Elevation', 'Temperature', 'Humidity', 'Soil_TN', 'Soil_TP', 'Fire_Risk_Index']]
+features = df[['Elevation', 'Humidity', 'Soil_TP']]
 
 labels = df[['Health_Status']]
 labels.columns = ['health_status']
